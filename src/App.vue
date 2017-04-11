4418c9bc41b2c91a35a33e8e90e1747f3957065e
yojotaman <yojotaman@gmail.com>
2017-04-10 23:55:03 -0500

    Fixes por la URL para publicacion en github pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://yojotaman.github.io/vue_kinetimusic/dist/logo.png')
     h1 KinetiMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{country.name}}

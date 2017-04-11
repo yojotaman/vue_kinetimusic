0cb997adf6303f812a1ac9b8ee140d75e56bf59f
yojotaman <yojotaman@gmail.com>
2017-04-10 22:18:27 -0500

    Configurando la api de last fm en Vue.js

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+
+export default {
+  appName: 'kinetimusic',
+  apiKey: '6722a9e8db476319cdf935a8eacd73dc',
+  secret: '53b2cea2dfbcdcbf42381a36ece40829',
+  registeredTo: 'yojotaman'
+}
+
+// Application name	kinetimusic
+// API key	6722a9e8db476319cdf935a8eacd73dc
+// Shared secret	53b2cea2dfbcdcbf42381a36ece40829
+// Registered to	yojotaman
+
